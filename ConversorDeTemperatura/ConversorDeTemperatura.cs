406f520de2a1c27f16fa7afc0ca3a509cabc4683
Caio-Menezes-Silva <caiomesi@gmail.com>
2025-05-26 16:13:19 -0300

    Adicionar arquivos de projeto.

---
+++ ConversorDeTemperatura/ConversorDeTemperatura.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
+
+namespace ConversorDeTemperatura
+{
+    internal class ConversorDeTemperatura
+    {
+        private double _temperaturaCelsius;
+        private double _temperaturaFahrenheit;
+        public double TemperaturaCelsius { get { return _temperaturaCelsius; } set { _temperaturaCelsius = value; } }
+        public double TemperaturaFahrenheit { get { return _temperaturaFahrenheit; } set { _temperaturaFahrenheit = value; } }
+
+        public ConversorDeTemperatura(double TemperaturaCelsius, double TemperaturaFahrenheit)
+        {
+             _temperaturaCelsius = TemperaturaCelsius ;
+             _temperaturaFahrenheit = TemperaturaFahrenheit;
+        }
+
+        public ConversorDeTemperatura()
+        {
+        }
+
+        public double CelsiusParaFahrenheit(double temperaturaCelsius)
+        {   TemperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32;
+
+            return TemperaturaFahrenheit;
+        }
+
+        public double FahrenheitParaCelsius(double temperaturaFahrenheit)
+        {
+            return TemperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9;
+        }
+
+
+    }
+}
---
+++ ConversorDeTemperatura/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+namespace ConversorDeTemperatura
+{
+    internal class MenuPrincipal
+    {
+        static void Main(string[] args)
+        {
+            ConversorDeTemperatura conversor = new ConversorDeTemperatura();
+            int option = -1;
+
+            do
+            {
+                Console.WriteLine("Bem-vindo ao Conversor de Temperatura!");
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Celsius para Fahrenheit" + "\n2 - Fahrenheit para Celsius" + "\n3 - Sair do programa");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Digite a temperatura em Celsius:");
+                        conversor.TemperaturaCelsius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conversor.CelsiusParaFahrenheit(conversor.TemperaturaCelsius);
+                        Console.WriteLine($"Temperatura em Fahrenheit: {conversor.TemperaturaFahrenheit}F° ");
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite a temperatura em Fahrenheit:");
+                        double temperaturaFahrenheit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        double resultadoCelsius = conversor.FahrenheitParaCelsius(temperaturaFahrenheit);
+                        Console.WriteLine($"Temperatura em Celsius: {resultadoCelsius.ToString("F2", CultureInfo.InvariantCulture)}°C");
+                        break;
+                    case 3:
+                        Console.WriteLine("Saindo do programa. Até logo!");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+                
+
+                if (option == 3)
+                {
+                    Console.WriteLine("Saindo do programa. Até logo!");
+                }
+
+            } while (option != 3);
+
+        }
+    }
+}
